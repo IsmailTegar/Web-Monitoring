45ae67ad5fd818a434dff8d0db06d12072ac39f9
garuhan <niawan013@gmail.com>
2025-08-29 14:06:37 +0700

    SettingAPI1

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RouterOsAPI;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $ip = '192.168.100.2';
+        $user = 'Ismail';
+        $pass = 'tegar';
+        $API = new RouterOsAPI();
+        $API->debug('false');
+
+        if($API->connect($ip , $user , $pass)) {
+            $identitas = $API->comm('/interface/print');
+        } else {
+            return'Koneksi Gagal';
+        }
+
+        dd($identitas);
+
+        return view('dashboard');
+    }
+}
