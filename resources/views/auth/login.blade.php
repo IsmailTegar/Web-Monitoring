3487ce724f882961107ec5b381991ecbd8f5e953
garuhan <niawan013@gmail.com>
2025-08-29 20:59:33 +0700

    Login part1

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+  </head>
+  <body>
+    <div class="login">
+      <div class="frame-wrapper">
+        <div class="frame">
+          <img class="rectangle" src="img/rectangle-3.png" />
+          <div class="div">
+            <div class="frame-2">
+              <div class="div-wrapper"><div class="text-wrapper">Welcome!</div></div>
+              <div class="frame-3">
+                <p class="p">
+                  Proin id ligula dictum, convallis enim ut, facilisis massa. Mauris a nisi ut sapien blandit imperdie
+                </p>
+              </div>
+            </div>
+            <div class="frame-4">
+              <div class="frame-5"><div class="text-wrapper-2">Login</div></div>
+              <div class="frame-6">
+                <div class="frame-7">
+                  <div class="input-box">
+                    <div class="frame-8"><div class="text-wrapper-3">IP Address</div></div>
+                    <input type="text" placeholder="Name" class="input-custom">
+                  </div>
+                  <div class="input-box">
+                    <div class="frame-8"><div class="text-wrapper-3">User Name</div></div>
+                    <div class="frame-9"><div class="text-wrapper-4">Name</div></div>
+                  </div>
+                  <div class="input-box">
+                    <div class="frame-8"><div class="text-wrapper-3">Password</div></div>
+                    <div class="frame-9"><div class="text-wrapper-4">Password</div></div>
+                  </div>
+                </div>
+                <button class="button-wrapper">
+                  <button class="button"><button class="button-2">Submit</button></button>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
